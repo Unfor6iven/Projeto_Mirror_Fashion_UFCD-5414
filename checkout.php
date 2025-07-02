f69d13594b708b529d9a37c7281e2f5aaaa7d203
Unfor6iven <58237238+Unfor6iven@users.noreply.github.com>
2025-07-02 13:59:59 +0100

    2025.07.02

--- checkout.php
+++ checkout.php
@@ -4,19 +4,74 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/bootstrap.css">
+
+    <link rel="stylesheet" href="CSS/bootstrap-flatly.css">
 
 
     <title>Checkout Mirror Fashion</title>
 
     <style>
+        body {
+            padding-top: 70px;
+        }
+
         .form-control:invalid {
             border: 2px solid #c00;
+        }
+
+        .mb-0 {
+            margin-bottom: 0;
+        }
+
+        .navbar .glyphicon {
+            color: yellow;
+        }
+
+        .navbar-brand img {
+            height: 30px;
         }
     </style>
 </head>
 
 <body>
+
+    <nav class="navbar navbar-default navbar-fixed-top mb-0">
+
+        <div class="container-fluid">
+
+            <div class="navbar-header">
+                <a href="index.php" class="navbar-brand">
+                    <img src="IMG/logo-rodape.png" alt="Logo">
+                </a>
+                <!-- data-target (precisa de indicação especifica do target, se é class . ou se é id # ou etc) -->
+                <!-- data-toggle (faz menção da função a chamar com JS) -->
+                <button type="button" class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse">
+                    <span class="glyphicon glyphicon-align-justify"></span>
+                </button>
+            </div>
+
+            <div class="collapse navbar-collapse">
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="sobre.php"><span class="glyphicon glyphicon-home"></span>&nbsp; Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="glyphicon glyphicon-question-sign"></span>&nbsp; Ajuda</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="glyphicon glyphicon-list-alt"></span>&nbsp; Perguntas Frequentes</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="glyphicon glyphicon-bullhorn"></span>&nbsp; Contactos</a>
+                    </li>
+                </ul>
+
+            </div>
+
+        </div>
+
+    </nav>
 
     <div class="jumbotron">
         <div class="container">
@@ -29,94 +84,105 @@
     </div> <!-- fim .jumbotron -->
 
     <div class="container">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h2 class="container">Sua compra: </h2>
-            </div> <!-- fim .panel-heading -->
 
-            <img src="IMG/produtos/foto<?= $_POST['id'] ?>-<?= $_POST['cor'] ?>.png" alt="Foto1" class="img-thumbnail img-responsive">
-            <div class="panel-body">
-                <dl>
-                    <dt>Produto: </dt>
-                    <dd><?= $_POST['nome'] ?></dd>
-                    <dt>Cor: </dt>
-                    <dd><?= $_POST['cor'] ?></dd>
-                    <dt>Tamanho: </dt>
-                    <dd><?= $_POST['tamanho'] ?></dd>
-                    <dt>Preço: </dt>
-                    <dd><?= $_POST['preco'] ?></dd>
-                </dl>
-            </div> <!-- fim .panel-body -->
-        </div> <!-- fim .panel -->
+        <div class="row">
 
-        <form action="#">
-            <fieldset>
-                <legend>Dados Pessoais</legend>
+            <div class="col-lg-3 col-sm-4 ">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h2 class="container">Sua compra: </h2>
+                    </div> <!-- fim .panel-heading -->
 
-                <div class="form-group">
-                    <label for="nome">Nome</label>
-                    <input type="text" class="form-control" name="nome" id="nome" placeholder="Nome Completo" autofocus required>
-                </div>
+                    <div class="panel-body">
 
-                <div class="form-group">
-                    <label for="email">Email address</label>
-                    <div class="input-group">
-                        <span class="input-group-addon">@</span>
-                        <input type="email" class="form-control" name="email" id="email" placeholder="email@exemplo.com">
+                        <img src="IMG/produtos/foto<?= $_POST['id'] ?>-<?= $_POST['cor'] ?>.png" alt="Foto1" class="img-thumbnail img-responsive hidden-xs">
+
+                        <dl>
+                            <dt>Produto: </dt>
+                            <dd><?= $_POST['nome'] ?></dd>
+                            <dt>Cor: </dt>
+                            <dd><?= $_POST['cor'] ?></dd>
+                            <dt>Tamanho: </dt>
+                            <dd><?= $_POST['tamanho'] ?></dd>
+                            <dt>Preço: </dt>
+                            <dd><?= $_POST['preco'] ?></dd>
+                        </dl>
+                    </div> <!-- fim .panel-body -->
+                </div> <!-- fim .panel -->
+            </div>
+
+            <div class="col-sm-8 col-lg-9">
+                <form action="#">
+
+                    <div class="row">
+
+                        <fieldset class="col-md-6">
+                            <legend>Dados Pessoais</legend>
+                            <div class="form-group">
+                                <label for="nome">Nome</label>
+                                <input type="text" class="form-control" name="nome" id="nome" placeholder="Nome Completo" autofocus required>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <div class="input-group">
+                                    <span class="input-group-addon">@</span>
+                                    <input type="email" class="form-control" name="email" id="email" placeholder="email@exemplo.com">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="nif">NIF</label>
+                                <div class="input-group">
+                                    <span class="input-group-addon">#</span>
+                                    <input type="text" class="form-control" name="nif" id="nif" placeholder="000 000 000" pattern="[0-9]{9}" required>
+                                </div>
+                            </div>
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="spam" checked value="sim">
+                                    Quero receber spam da Mirror Fashion
+                                </label>
+                            </div>
+                        </fieldset>
+
+                        <fieldset class="col-md-6">
+                            <legend>Cartão de Crédito</legend>
+
+                            <div class="form-group">
+                                <label for="cardbanner">Tipo</label>
+                                <select name="cardbanner" id="cardbanner">
+                                    <option value="master">MasterCard</option>
+                                    <option value="visa">Visa</option>
+                                    <option value="amex">American Express</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="cardnum">Número do Cartão</label>
+                                <input type="text" class="form-control" name="cardnum" id="cardnum" placeholder="0000 0000 0000 0000" pattern="[0-9]{12}">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="cardcvv">CVV</label>
+                                <input type="text" class="form-control" name="cardcvv" id="cardcvv" placeholder="CVV" pattern="[0-9]{3}">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="cardval">Validade</label>
+                                <input type="month" class="form-control" name="cardval" id="cardval">
+                            </div>
+                        </fieldset>
+
                     </div>
-                </div>
+                    <button type="submit" class="btn btn-primary btn-lg pull-right">
+                        <i class="glyphicon glyphicon-thumbs-up"></i>
+                        Confirmar Pedido
+                    </button>
+                </form>
+            </div>
 
-                <div class="form-group">
-                    <label for="nif">NIF</label>
-                    <div class="input-group">
-                        <span class="input-group-addon">#</span>
-                        <input type="text" class="form-control" name="nif" id="nif" placeholder="000 000 000" pattern="[0-9]{9}" required>
-                    </div>
-                </div>
+        </div> <!-- fim da .row -->
 
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" name="spam" checked value="sim">
-                        Quero receber spam da Mirror Fashion
-                    </label>
-                </div>
-            </fieldset>
-
-            <fieldset>
-                <legend>Cartão de Crédito</legend>
-
-                <div class="form-group">
-                    <label for="cardnum">Número do Cartão</label>
-                    <input type="text" class="form-control" name="cardnum" id="cardnum" placeholder="0000 0000 0000 0000" pattern="[0-9]{12}">
-                </div>
-
-                <div class="form-group">
-                    <label for="cardcvv">CVV</label>
-                    <input type="text" class="form-control" name="cardcvv" id="cardcvv" placeholder="CVV" pattern="[0-9]{3}">
-                </div>
-
-                <div class="form-group">
-                    <label for="cardbanner">Bandeira</label>
-                    <select name="cardbanner" id="cardbanner">
-                        <option value="master">MasterCard</option>
-                        <option value="visa">Visa</option>
-                        <option value="amex">American Express</option>
-                    </select>
-
-                </div>
-
-                <div class="form-group">
-                    <label for="cardval">Validade</label>
-                    <input type="month" class="form-control" name="cardval" id="cardval">
-                </div>
-            </fieldset>
-
-            <button type="submit" class="btn btn-primary btn-lg pull-right">
-                <i class="glyphicon glyphicon-thumbs-up"></i>
-                Confirmar Pedido
-            </button>
-        </form>
-    </div>
+    </div> <!-- fim do .container -->
 
     <script>
         document.querySelector('input[type=email]').oninvalid = function() {
@@ -124,12 +190,16 @@
             this.setCustomValidity("");
 
             // reexecutar validação
-            if(!this.validity.valid) {
+            if (!this.validity.valid) {
                 //se inválido, coloca mensagem de erro
                 this.setCustomValidity("Email Inválido!");
             }
         }
     </script>
+
+    <script src="JS/jquery.js"></script>
+    <script src="JS/bootstrap.js"></script>
+
 </body>
 
 </html>
